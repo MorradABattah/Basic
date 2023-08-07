4c81f346e75970ddac6932991433b4e1214f098c
MorradABattah <morradbattah1995@gmail.com>
2023-08-07 15:25:15 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,17 @@
 import unittest
 from flask import url_for
-from runner import app
+from runner import app  # replace 'your_flask_app' with the name of your Flask application module
 
 class TestApp(unittest.TestCase):
-
     def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SERVER_NAME'] = 'localhost:5000'
         self.client = app.test_client()
-        self.client.testing = True
-        self.ctx = app.app_context()
-        self.ctx.push()
-
-    def tearDown(self):
-        self.ctx.pop()
 
     def test_index(self):
-        response = self.client.get(url_for('index'))
-        self.assertEqual(response.status_code, 200)
+        with app.app_context():
+            response = self.client.get(url_for('index'))
+            self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
